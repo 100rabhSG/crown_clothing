ff3441c5c23f1d97442d1f0233b973b6623f9962
Saurabh <saurabh.agrawal.1605@gmail.com>
2024-02-15 13:12:16 +0530

    first commit

---
+++ src/components/category-item/Category-item.component.jsx
@@ -0,0 +1,18 @@
+import './Category-item.styles.scss';
+
+const CateogyItem = ({ category }) => {
+    const { id, title, imageUrl } = category;
+    return(
+        <div key={id} className="category-container">
+            <div className="background-image" style={{
+            backgroundImage: `url(${imageUrl})`
+            }} />
+            <div className="category-body-container">
+            <h2> {title} </h2>
+            <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CateogyItem;
