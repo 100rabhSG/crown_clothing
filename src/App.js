232716bc25b9e15645b888e049124f04ab509e20
Saurabh <saurabh.agrawal.1605@gmail.com>
2024-02-15 16:25:55 +0530

    setting up basic routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import Directory from './components/directory/Directory.component'
+import { Routes, Route, Outlet } from 'react-router-dom'; 
 
+import Home from './Routes/Home/Home.component';
+import Navigation from './Routes/Navigation/Navigation.component';
 import './categories.styles.scss'
 
-const App = () => {
 
+
+const  Shop = () => {
   return(
-    <Directory/>
+    <h1> I am shop component</h1>
+  )
+}
+
+const App = () => {
+  return(
+    <Routes>
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   )
 }
 
