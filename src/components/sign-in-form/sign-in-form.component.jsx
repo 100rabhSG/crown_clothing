472503b1f62ffafb33f6fb2481c4ec30c0239d90
Saurabh <saurabh.agrawal.1605@gmail.com>
2024-02-21 17:12:17 +0530

    centralized the authentication

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,7 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-
-import { UserContext } from "../../contexts/user.context";
-
 
 import {
   signInWithGooglePopup,
@@ -23,16 +20,13 @@ const SingInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    setCurrentUser(user);
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
+
   };
 
   const handleSumit = async (event) => {
@@ -43,7 +37,6 @@ const SingInForm = () => {
         email,
         password
       );
-      setCurrentUser(user);
       
       resetFormFields();
     } catch (error) {
