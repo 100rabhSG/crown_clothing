96d089bb67fe734bf5e5a795159280270f1903a7
Saurabh <saurabh.agrawal.1605@gmail.com>
2024-02-22 17:50:18 +0530

    added checkout page and checkout functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Routes/Home/Home.component';
 import Navigation from './Routes/Navigation/Navigation.component';
 import Authentication from './Routes/Authentication/authentication.component';
 import Shop from './Routes/shop/shop.component';
+import Checkout from './Routes/Checkout/checkout.component';
 
 import './categories.styles.scss'
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   )
